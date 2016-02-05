ae38f7ff4dc2da655501a8a706efddfc103bd983
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2016-02-05 20:51:44 -0200

    added apt-get to basic libraries and fixed the 'move' vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
-
+apt-get install -y curl wget vim
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
 
 # Move .vim folder to user folder
-mv .vim ~/
+mv .vimrc ~/
 
